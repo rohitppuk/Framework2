d614889ba3b54e8417da3007f9712bf8e3251801
Rohit <Rohitppuk@gmail.com>
2022-06-23 20:38:21 +0100

    homework

--- NopCommerceFramework.java
+++ NopCommerceFramework.java
@@ -52,8 +52,8 @@ public class NopCommerceFramework {
         Select select = new Select(country);
         select.selectByVisibleText("United States");
        Thread.sleep(2000);
-    WebElement county = driver.findElement(By.xpath("/html/body/div[6]/div[3]/div/div/div/div[2]/ol/li[1]/div[2]/form/div/div/div[2]/div/div/div[6]/select"));
-    Select select1 = new Select(county);
+       WebElement county = driver.findElement(By.xpath("/html/body/div[6]/div[3]/div/div/div/div[2]/ol/li[1]/div[2]/form/div/div/div[2]/div/div/div[6]/select"));
+        Select select1 = new Select(county);
         select1.selectByVisibleText("Alaska");
 
         driver.findElement(By.id("BillingNewAddress_City")).sendKeys("NewYork");
@@ -61,10 +61,45 @@ public class NopCommerceFramework {
         driver.findElement(By.id("BillingNewAddress_ZipPostalCode")).sendKeys("nw13pq");
         driver.findElement(By.id("BillingNewAddress_PhoneNumber")).sendKeys("420840");
         driver.findElement(By.xpath("//button[@type='button'][@class='button-1 new-address-next-step-button']")).click();
-        driver.findElement(By.xpath("//button[@class='button-1 shipping-method-next-step-button']")).click();
+
     }
+@Test(priority = 5)
+void shippingMethod() throws InterruptedException {
 
+    Thread.sleep(2000);
+    driver.findElement(By.xpath("//input[@id='shippingoption_1'][@value='Next Day Air___Shipping.FixedByWeightByTotal']")).click();
+    driver.findElement(By.xpath("//button[@class='button-1 shipping-method-next-step-button'][@onclick='ShippingMethod.save()']")).click();
+    Thread.sleep(2000);
+    driver.findElement(By.id("paymentmethod_1")).click();
+    Thread.sleep(2000);
+    driver.findElement(By.xpath("//button[@class='button-1 payment-method-next-step-button'][@onclick='PaymentMethod.save()']")).click();
+}
+@Test(priority = 6)
+void paymentInfo() throws InterruptedException {
+    Thread.sleep(2000);
+    WebElement card = driver.findElement(By.id("CreditCardType"));
+    Select selectCard = new Select(card);
+    selectCard.selectByIndex(1);
+    driver.findElement(By.id("CardholderName")).sendKeys("I AM TESTER KUMAR");
+    driver.findElement(By.id("CardNumber")).sendKeys("0000 0000 0000 0000");
+    WebElement month =  driver.findElement(By.id("ExpireMonth"));
+    Select selectMonth = new Select(month);
+    Thread.sleep(2000);
+    selectMonth.selectByValue("5");
+    WebElement year=  driver.findElement(By.id("ExpireYear"));
+    Select selectYear = new Select(year);
+    Thread.sleep(2000);
+    selectYear.selectByValue("2024");
+    driver.findElement(By.id("CardCode")).sendKeys("333");
 
+    Thread.sleep(2000);
+     driver.findElement(By.xpath("//button[@class='button-1 payment-info-next-step-button'][@onclick='PaymentInfo.save()']")).click();
+    Thread.sleep(2000);
+    driver.findElement(By.xpath("//button[@class='button-1 confirm-order-next-step-button'][@onclick='ConfirmOrder.save()']")).click();
+    Thread.sleep(2000);
+    driver.findElement(By.xpath("//button[@class='button-1 order-completed-continue-button'][@onclick='setLocation(\"/\")']")).click();
+    Thread.sleep(1000);
+    driver.close();
 
-
+}
 }
